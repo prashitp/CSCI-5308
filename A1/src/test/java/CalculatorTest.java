0c28715b8d6a3e5f290f7f09978849893f5c8306
Prashit Patel <prashitppatel@gmail.com>
2021-10-07 02:16:07 +0530

    step-1 Calculator class exist

---
+++ A1/src/test/java/CalculatorTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculatorTest {
+
+    @Test
+    public void classNotNullTest() {
+        Calculator c = new Calculator();
+        assertNotNull(c);
+    }
+
+}
