5ba923a5f191546fc36cb6c96b020126639e6f1f
Prashit Patel <prashitppatel@gmail.com>
2021-10-07 04:40:05 +0530

    step2 - added add method

--- A1/src/test/java/CalculatorTest.java
+++ A1/src/test/java/CalculatorTest.java
@@ -10,4 +10,18 @@ public class CalculatorTest {
         assertNotNull(c);
     }
 
-}
+    @Test
+    public void addNegativeTest() {
+        Calculator c = new Calculator();
+        int ans = c.add(-4,-2);
+        assertEquals(-6,-6);
+    }
+
+    @Test
+    public void addPositiveTest() {
+        Calculator c = new Calculator();
+        int ans = c.add(4,2);
+        assertEquals(6,6);
+    }
+
+ }
