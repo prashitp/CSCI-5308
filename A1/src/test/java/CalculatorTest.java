df1feac4fcbd2fcbe57ed375d18040c83ac693af
Prashit Patel <prashitppatel@gmail.com>
2021-10-07 06:04:20 +0530

    step3 - added subtract, multiply, divide methods

--- A1/src/test/java/CalculatorTest.java
+++ A1/src/test/java/CalculatorTest.java
@@ -14,14 +14,42 @@ public class CalculatorTest {
     public void addNegativeTest() {
         Calculator c = new Calculator();
         int ans = c.add(-4,-2);
-        assertEquals(-6,-6);
+        assertEquals(ans,-6);
     }
 
     @Test
     public void addPositiveTest() {
         Calculator c = new Calculator();
         int ans = c.add(4,2);
-        assertEquals(6,6);
+        assertEquals(ans,6);
+    }
+
+    @Test
+    public void subtractNegativeTest() {
+        Calculator c = new Calculator();
+        int ans = c.subtract(-4,-2);
+        assertEquals(ans,-2);
+    }
+
+    @Test
+    public void subtractPositiveTest() {
+        Calculator c = new Calculator();
+        int ans = c.subtract(4,2);
+        assertEquals(ans,2);
+    }
+
+    @Test
+    public void multiplyTest() {
+        Calculator c = new Calculator();
+        int ans = c.multiply(5,2);
+        assertEquals(ans,10);
+    }
+
+    @Test
+    public void divideTest() {
+        Calculator c = new Calculator();
+        int ans = c.divide(10, 2);
+        assertEquals(ans, 5);
     }
 
  }
