7cdf5350282a67ff297ae4cdab1b087be6f187fe
Prashit Patel <prashitppatel@gmail.com>
2021-10-07 08:53:23 +0530

    step10 - adding validate method

--- A1/src/main/java/Validation.java
+++ A1/src/main/java/Validation.java
@@ -1,3 +1,15 @@
-public class Validation {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Validation {
+    public List<String> validate(String... values) {
+        List<String> errorArray = new ArrayList<String>();
+        for(String value: values){
+            String[] forValue = value.split("\\.");
+            if(forValue[1].length() > 2) {
+                errorArray.add("Decimal Exceeded for " + value);
+            }
+        }
+        return errorArray;
+    }
 }
--- A1/src/test/java/ValidationTest.java
+++ A1/src/test/java/ValidationTest.java
@@ -1,5 +1,7 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public  class ValidationTest {
@@ -9,4 +11,12 @@ public  class ValidationTest {
         Validation v = new Validation();
         assertNotNull(v);
     }
+
+    @Test
+    public void validateTest() {
+        Validation v = new Validation();
+        List<String> ans = v.validate("1.11","2.321");
+        assertEquals("Decimal Exceeded for 2.321",ans.get(0));
+    }
+
 }
