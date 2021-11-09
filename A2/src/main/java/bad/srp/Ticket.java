e96e335ef2097a9810551c0bde6a1ff61e6ab4c8
Prashit Prakashbhai Patel <pr718986@dal.ca>
2021-11-09 00:34:53 -0400

    bad-srp

---
+++ A2/src/main/java/bad/srp/Ticket.java
@@ -0,0 +1,25 @@
+package bad.srp;
+
+public class Ticket {
+	public enum Category {
+		Silver,Gold,Platinum
+	};
+
+	public enum Status {
+		available, blocked, booked
+	};
+
+	String seatNo;
+	Category category;
+	Status status;
+	int price;
+	boolean payment;
+
+	Ticket(String seatNo, Category category, int price) {
+		this.seatNo = seatNo;
+		this.category = category;
+		this.status = Status.available;
+		this.price = price;
+		this.payment = false;
+	}
+}
